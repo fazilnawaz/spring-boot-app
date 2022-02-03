d304023aaa78d065fd4ae42047c988a529ee0dd7
fazilnawaz <70374323+fazilnawaz@users.noreply.github.com>
2022-02-03 23:38:59 +0500

    Add files via upload

---
+++ src/main/java/com/fazil/fazilk8s/FazilK8sApplication.java
@@ -0,0 +1,13 @@
+package com.fazil.fazilk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FazilK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FazilK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fazil/fazilk8s/FazilK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazil.fazilk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FazilK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
